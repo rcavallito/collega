d7b25b54e7c125a79f534901e52e5d43b318d2d4
Gloop Media <Gloop Media>
2019-06-22 16:59:40 -0400

    Addition of questionnaire screens #2 and #3 + push to Firebase + read from Firebase

---
+++ Collega/Questionnaire3ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Questionnaire3ViewController.swift
+//  Collega
+//
+//  Created by Robert Cavallito on 6/20/19.
+//  Copyright © 2019 Gloop Media. All rights reserved.
+//
+
+import UIKit
+
+class Questionnaire3ViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
